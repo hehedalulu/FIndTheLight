4abc212f3dba60bc143e48b00ad05eea0f598ba7
王璐璐 <982472770@qq.com>
2016-11-25 15:20:13 +0800

    Initial Commit

---
+++ FIndTheLight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FIndTheLight
+//
+//  Created by Wll on 16/11/25.
+//  Copyright © 2016年 CherryWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FIndTheLight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FIndTheLight
+//
+//  Created by Wll on 16/11/25.
+//  Copyright © 2016年 CherryWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
